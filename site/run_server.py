6fd4b9c8b2df2095e5ca629dab14a856e018de3b
Demiu <miazgapow@gmail.com>
2021-11-24 23:13:08 +0100

    Create server script

---
+++ site/run_server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map={
+    '.manifest': 'text/cache-manifest',
+	'.html': 'text/html',
+    '.png': 'image/png',
+	'.jpg': 'image/jpg',
+	'.svg':	'image/svg+xml',
+	'.css':	'text/css',
+	'.js':	'application/x-javascript',
+	'': 'application/octet-stream', # Default
+}
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+try:
+    print(f"serving at port {PORT}")
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
